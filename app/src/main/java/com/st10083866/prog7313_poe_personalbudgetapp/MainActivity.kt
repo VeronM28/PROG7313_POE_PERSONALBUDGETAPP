d4cdbe6255ced17ddd695bc6c16c1734dda1e750
VeronM28 <veronmspare2809@gmail.com>
2025-04-23 18:46:09 +0200

    Initial commit

---
+++ app/src/main/java/com/st10083866/prog7313_poe_personalbudgetapp/MainActivity.kt
@@ -0,0 +1,20 @@
+package com.st10083866.prog7313_poe_personalbudgetapp
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}

9f1c2cfa43cf794d8e980336eeec1535cad2a78b
VeronM28 <veronmspare2809@gmail.com>
2025-04-28 01:39:44 +0200

    added backend for launch page

--- app/src/main/java/com/st10083866/prog7313_poe_personalbudgetapp/LaunchPageActivity.kt
+++ app/src/main/java/com/st10083866/prog7313_poe_personalbudgetapp/LaunchPageActivity.kt
@@ -1,20 +1,27 @@
 package com.st10083866.prog7313_poe_personalbudgetapp
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class LaunchPageActivity : AppCompatActivity() {
+
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_launchpage)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+        val binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.btn_launch_login {
+            val intent = Intent(this, LoginPage::class.java)
+            startActivity(intent)
         }
     }
 }

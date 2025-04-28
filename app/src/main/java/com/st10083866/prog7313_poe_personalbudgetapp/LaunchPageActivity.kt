756f8feb7b78bdc055b2997d946b88e31934159a
VeronM28 <veronmspare2809@gmail.com>
2025-04-28 16:44:34 +0200

    ammended backend for launchpageactivity

--- app/src/main/java/com/st10083866/prog7313_poe_personalbudgetapp/LaunchPageActivity.kt
+++ app/src/main/java/com/st10083866/prog7313_poe_personalbudgetapp/LaunchPageActivity.kt
@@ -2,26 +2,36 @@ package com.st10083866.prog7313_poe_personalbudgetapp
 
 import android.content.Intent
 import android.os.Bundle
+import android.view.View
 import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.st10083866.prog7313_poe_personalbudgetapp.databinding.ActivityLaunchpageBinding
 
 class LaunchPageActivity : AppCompatActivity() {
 
-
+    private lateinit var binding: ActivityLaunchpageBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_launchpage)
-        val binding = ActivityMainBinding.inflate(layoutInflater)
+        binding = ActivityLaunchpageBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        binding.btn_launch_login {
+        binding.btnLaunchLogin.setOnClickListener {
             val intent = Intent(this, LoginPage::class.java)
             startActivity(intent)
         }
+
+        binding.btnLaunchReg.setOnClickListener {
+            val intent = Intent(this, RegActivity::class.java)
+            startActivity(intent)
+        }
+
+
+
     }
 }

81f3940bb32a75d65a7e0b24afc1c913b822b930
VeronM28 <veronmspare2809@gmail.com>
2025-05-01 19:17:42 +0200

    created fragment for dashboard

--- app/src/main/java/com/st10083866/prog7313_poe_personalbudgetapp/Dashboard.kt
+++ app/src/main/java/com/st10083866/prog7313_poe_personalbudgetapp/Dashboard.kt
@@ -10,7 +10,7 @@ class Dashboard : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_dashboard)
+        setContentView(R.layout.fragment_dashboard)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
